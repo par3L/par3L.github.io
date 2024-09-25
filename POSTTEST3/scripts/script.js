d901a56bb42df73dcb1f11abb605d65a77b219d8
Farrel <farrel.farrelputra@outlook.com>
2024-09-25 20:38:28 +0800

    web
    ee pusing sudaa, masuk goa dlu belajar lebih dalam

---
+++ POSTTEST3/scripts/script.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector('.hamburger');
+const dropMenu = document.querySelector('.drop-menu');
+const modeToggle = document.querySelector('.mode-toggle');
+const walkInServiceLink = document.querySelector('a[href="#walk-in"]');
+const shopServiceLink = document.querySelector('a[href="#shop"]');
+
+
+const body = document.body;
+
+hamburger.addEventListener('click', () => {
+    dropMenu.classList.toggle('active');
+});
+
+modeToggle.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+    const modeIcon = modeToggle.querySelector('i');
+    const modeText = modeToggle.querySelector('span');
+
+    if (body.classList.contains('light-mode')) {
+        modeIcon.classList.replace('fa-moon', 'fa-sun');
+        modeText.textContent = "Light Mode";
+    } else {
+        modeIcon.classList.replace('fa-sun', 'fa-moon');
+        modeText.textContent = "Dark Mode";
+    }
+});
+
+walkInServiceLink.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    alert('The Walk-in Service feature is currently under development.');
+});
+
+shopServiceLink.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    alert('The Shop feature is currently under development.');
+});
