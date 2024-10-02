e706695a118a156c51119b04b89f909f06a84037
Farrel <farrel.farrelputra@outlook.com>
2024-10-02 20:15:03 +0800

    Add files via upload

---
+++ POSTTEST4/navbar.php
@@ -0,0 +1,23 @@
+    <header>
+        <div class="navbar">
+            <div class="logo"><a href="#">Bengkuring Techsperts</a></div>
+            <ul class="links">
+                <li class="nav-items"><a href="./index.php">Home</a></li>
+                <li class="nav-items"><a href="#about">About Us</a></li>
+                <li class="nav-items"><a href="./mail.php">Services</a></li>
+                <li class="nav-items"><a href="#contact">Contact Us</a></li>
+            </ul>
+            <div class="hamburger">
+                <i class="fa-solid fa-bars"></i>    
+            </div>
+            <div class="mode-toggle"></div>
+                <i class="fa-solid fa-moon"></i>    
+            </div>
+        </div>
+        <div class="drop-menu">
+            <li class="nav-items"><a href="./index.php">Home</a></li>
+            <li class="nav-items"><a href="#about">About Us</a></li>
+            <li class="nav-items"><a href="./mail.php">Services</a></li>
+            <li class="nav-items"><a href="#contact">Contact Us</a></li>
+        </div>
+    </header>
